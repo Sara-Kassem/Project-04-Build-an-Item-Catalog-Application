ad2ffc81df0a400bff40a6db8288d0b4a2a1e183
Sara-Kassem <sara.kasem@azhar.edu.eg>
2018-02-08 05:16:29 +0200

    Add "Crushed Lentil Soup with Lamb Meatballs" Ingredients

--- addRecipes.py
+++ addRecipes.py
@@ -104,3 +104,313 @@ lentil_soup = Recipe(
 
 session.add(lentil_soup)
 session.commit()
+
+# ------------------------------------------------------- #
+#   Crushed Lentil Soup with Lamb Meatballs Ingredients   #
+# ------------------------------------------------------- #
+
+ingredient001 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='1',
+    ingredient='crushed Lentil Soup'
+    )
+
+session.add(ingredient001)
+session.commit()
+
+ingredient002 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='2',
+    ingredient='2T. extra virgin Olive oil'
+    )
+
+session.add(ingredient002)
+session.commit()
+
+ingredient003 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='3',
+    ingredient='1 lg. yellow onion, diced (about 2 cups)'
+    )
+
+session.add(ingredient003)
+session.commit()
+
+ingredient004 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='4',
+    ingredient='1 lg. carrot, diced (about 1/2 cup)'
+    )
+
+session.add(ingredient004)
+session.commit()
+
+ingredient005 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='5',
+    ingredient='1 celery stalk, diced (about 1/2 cup)'
+    )
+
+session.add(ingredient005)
+session.commit()
+
+ingredient006 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='6',
+    ingredient='5 garlic cloves, minced'
+    )
+
+session.add(ingredient006)
+session.commit()
+
+ingredient007 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='7',
+    ingredient='3 fresh bay leaves'
+    )
+
+session.add(ingredient007)
+session.commit()
+
+ingredient008 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='8',
+    ingredient='1/2 c. red split lentils'
+    )
+
+session.add(ingredient008)
+session.commit()
+
+ingredient009 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='9',
+    ingredient='1/2 c. brown rice'
+    )
+
+session.add(ingredient009)
+session.commit()
+
+ingredient010 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='10',
+    ingredient='2 1/2 tsp. curry powder'
+    )
+
+session.add(ingredient010)
+session.commit()
+
+ingredient011 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='11',
+    ingredient='1 1/2 tsp. sea salt'
+    )
+
+session.add(ingredient011)
+session.commit()
+
+ingredient012 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='12',
+    ingredient='1/4 tsp. crushed red pepper'
+    )
+
+session.add(ingredient012)
+session.commit()
+
+ingredient013 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='13',
+    ingredient='1/2 tsp. black pepper'
+    )
+
+session.add(ingredient013)
+session.commit()
+
+ingredient014 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='14',
+    ingredient='7 c. vegetable broth, divided'
+    )
+
+session.add(ingredient014)
+session.commit()
+
+ingredient015 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='15',
+    ingredient='Lamb Meatballs'
+    )
+
+session.add(ingredient015)
+session.commit()
+
+ingredient016 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='16',
+    ingredient='3 T. olive oil'
+    )
+
+session.add(ingredient016)
+session.commit()
+
+ingredient017 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='17',
+    ingredient='2 slices Udis Whole Grain Gluten Free Bread, crumbled'
+    )
+
+session.add(ingredient017)
+session.commit()
+
+ingredient018 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='18',
+    ingredient='1/4 c. organic whole milk'
+    )
+
+session.add(ingredient018)
+session.commit()
+
+ingredient019 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='19',
+    ingredient='1 lb. grassfed ground lamb'
+    )
+
+session.add(ingredient019)
+session.commit()
+
+ingredient020 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='20',
+    ingredient='1 c. slivered and toasted almonds, chopped'
+    )
+
+session.add(ingredient020)
+session.commit()
+
+ingredient021 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='21',
+    ingredient='1 smallish yellow onion (about &frac13; cup)'
+    )
+
+session.add(ingredient021)
+session.commit()
+
+ingredient022 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='22',
+    ingredient='1/3 c. golden raisins, chopped'
+    )
+
+session.add(ingredient022)
+session.commit()
+
+ingredient023 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='23',
+    ingredient='1 beaten egg'
+    )
+
+session.add(ingredient023)
+session.commit()
+
+ingredient024 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='24',
+    ingredient='3 garlic cloves, minced'
+    )
+
+session.add(ingredient024)
+session.commit()
+
+ingredient025 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='25',
+    ingredient='2 T. fresh chopped parsley'
+    )
+
+session.add(ingredient025)
+session.commit()
+
+ingredient026 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='26',
+    ingredient='1 T. fresh chopped cilantro'
+    )
+
+session.add(ingredient026)
+session.commit()
+
+ingredient027 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='27',
+    ingredient='1 tsp. kosher salt'
+    )
+
+session.add(ingredient027)
+session.commit()
+
+ingredient028 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='28',
+    ingredient='Yogurt Sauce'
+    )
+
+session.add(ingredient028)
+session.commit()
+
+ingredient029 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='29',
+    ingredient='1 c. organic plain yogurt'
+    )
+
+session.add(ingredient029)
+session.commit()
+
+ingredient030 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='30',
+    ingredient='1 T. chopped mint'
+    )
+
+session.add(ingredient030)
+session.commit()
+
+ingredient031 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='31',
+    ingredient='1/2 tsp. lemon zest'
+    )
+
+session.add(ingredient031)
+session.commit()
+
+ingredient032 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='32',
+    ingredient='1/2 tsp. lemon juice'
+    )
+
+session.add(ingredient032)
+session.commit()
+
+ingredient033 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='33',
+    ingredient='1 tsp. honey (optional since I gave up sugar)'
+    )
+
+session.add(ingredient033)
+session.commit()
+
+ingredient034 = Ingredients(
+    recipe_id=recipe001.id,
+    ingr_id='34',
+    ingredient='Pinch of salt'
+    )
+
+session.add(ingredient034)
+session.commit()
